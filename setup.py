24bc47a94487dc523fa807ff26b6bc6633f79921
Robert DeJaco <dejac001@umn.edu>
2020-05-21 23:32:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gasthermo",
-    version="0.0.4",
+    version="0.0.5",
     author="Robert F. De Jaco",
     author_email="dejac001@umn.edu",
     description="Gas-Phase Thermodynamics in Python",
