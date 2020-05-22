2f65cc0d8ee5cc4d9b545b19b8f66f28f6648fe8
Robert DeJaco <dejac001@umn.edu>
2020-05-21 23:32:23 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ copyright = '2020, Robert F. De Jaco'
 author = 'Robert F. De Jaco'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
-version = '0.0.4'
+release = '0.0.5'
+version = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
